25094fe47cbcffa7d079163b1defe9bfc92dbe6f
HS <hussein.salman@avanade.com>
2020-01-02 17:51:38 -0500

    add ingress files

---
+++ k8/Ingress/ingress-update-dns.sh
@@ -0,0 +1,11 @@
+# Public IP address
+IP="[INGRESS PUBLIC IP]"
+
+# Name to associate with public IP address
+DNSNAME="products-store-ingress"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with dns name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
